4ef59ca65d9a6b626ec9d738269206257f1cfe7d
syifshab73@gmail.com <syifshab73@gmail.com>
2025-09-03 11:48:41 +0700

    ubah ubah

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/_components/form.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { ActionResult } from "@/types";
+import { Label } from "@radix-ui/react-label";
+import React from "react";
+import { useFormState } from "react-dom";
+import { SignIn } from "../lib/actions";
+
+const initialState: ActionResult = {
+  error: "",
+};
+
+export default function FormSignIn({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [state, formAction] = useFormState(SignIn, initialState);
+  return (
+    <div className={cn("flex flex-col gap-6 w-[400px]", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={formAction}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input name="password" id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
